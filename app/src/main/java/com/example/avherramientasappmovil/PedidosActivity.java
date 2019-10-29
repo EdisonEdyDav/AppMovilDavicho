2e3ba58819f4f6b29de0c9c0efbb06769719ec40
EdisonEdyDav <davoedd@gmail,com>
2019-10-29 04:52:43 -0500

    inicio

--- app/src/main/java/com/example/avherramientasappmovil/PedidosActivity.java
+++ app/src/main/java/com/example/avherramientasappmovil/PedidosActivity.java
@@ -1,21 +1,36 @@
 package com.example.avherramientasappmovil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class PedidosActivity extends AppCompatActivity {
 
+    Button btn_IngresoPedidos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedidos);
 
-        Button btn_historial;
+        final Button btn_IngresoPedidos= (Button)findViewById(R.id.btn_IngresoPedidos);
+        btn_IngresoPedidos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(PedidosActivity.this, IngresoPedidosActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
 
 
         ArrayList<Fuente>lista= new ArrayList<Fuente>();
